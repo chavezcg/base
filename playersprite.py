9040d48d6859704592f2d1f1323d3742e6f4f980
chavezcg <catherinegchavez289@gmail.com>
2023-01-12 14:21:03 -0800

    we added animation to morty left arrow press

--- playersprite.py
+++ playersprite.py
@@ -10,7 +10,7 @@ class Player(pygame.sprite.Sprite):
         self.base_image = image
         self.rect = self.image.get_rect()
         self.image.set_colorkey((0, 0, 0))
-
+        self.image_index = 0
 
 
         self.rect.x = x
@@ -19,27 +19,35 @@ class Player(pygame.sprite.Sprite):
         self.screen = screen
         self.screenwidth = screen.get_width()
         self.screenheight = screen.get_height()
-        self.leftFacingList = left_images_list
-        self.right_images = up_images_list
-        self.left_images = up_images_list
+        self.leftFacingList = left_image_list
+        self.up_image = up_image_list
+        self.left_image = left_image_list
+        self.image_position = 0
 
     def flip_images(self, image_list):
         flipped_images = []
         for image in image_list:
-            flipped_images = pygame.transform.flip(image, True, False)
-            flipped_images.append(flipped_images)
+            flipped_image = pygame.transform.flip(image, True, False)
+            flipped_images.append(flipped_image)
         return flipped_images
 
 
     def update(self):
         self.image.set_colorkey((0, 0, 0))
         keys = pygame.key.get_pressed()
+
         if keys[pygame.K_LEFT]:
+            print(self.image_index)
             self.rect.x = self.rect.x - self.speed
-            self.image = self.leftFacingList[0]
+            if self.image == self.left_image[3]:
+                self.image_index = 0
+            self.image_index = self.image_index + 1
+            self.image = self.left_image[self.image_index]
 
         if keys[pygame.K_RIGHT]:
             self.rect.x = self.rect.x + self.speed
+            flipped_images = self.flip_images(self.left_image)
+            self.image = flipped_images[0]
 
         if keys[pygame.K_DOWN]:
             self.rect.y = self.rect.y + self.speed
@@ -47,7 +55,7 @@ class Player(pygame.sprite.Sprite):
 
         if keys[pygame.K_UP]:
             self.rect.y = self.rect.y - self.speed
-            self.image = self.up_images[0]
+            self.image = self.up_image[0]
 
         if self.rect.right > self.screenwidth:
             self.rect.right = self.screenwidth
