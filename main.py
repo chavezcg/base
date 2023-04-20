cc51a43b2caad705e3934e459ac6c29ddc868c5e
chavezcg <catherinegchavez289@gmail.com>
2023-04-20 13:22:48 -0700

    added gravity function to simulate gravity added background music to game

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 900
 #hello
 # loading images into pygame
-bg_img = pygame.image.load('pictures/spacebg.jpg')
+
+
 
 
 
@@ -31,6 +32,10 @@ class Game:
 
     def __init__(self):
         pygame.init()  # initialize pygame
+        self.platform_name
+        self.bg_music = pygame.mixer.music.load('music/bg_music.mp3')
+        pygame.mixer.music.play(-1)
+        self.bg_img = pygame.image.load('pictures/spacebg.jpg')
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # create screen
         self.clock = pygame.time.Clock()  # create clock
         self.running = True  # game loop
@@ -68,18 +73,70 @@ class Game:
         self.player_group = pygame.sprite.Group()
         self.player = ps.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 79, 113, self.screen, self.player_base_image, self.leftFacingList, self.upFacingList)
         self.object_group = pygame.sprite.Group()
-        self.test_object = os.ObjectSprite(self.player_base_image, 100, 100)
+        self.platform = os.ObjectSprite(self.player_base_image, 200, SCREEN_HEIGHT - 20, "platform")
+        self.platform2 = os.ObjectSprite(self.player_base_image, 300, SCREEN_HEIGHT - 40, "platform")
+        self.platform3 = os.ObjectSprite(self.player_base_image, 400, SCREEN_HEIGHT - 60, "platform")
+        self.platform4 = os.ObjectSprite(self.player_base_image, 500, SCREEN_HEIGHT - 80, "platform")
+        self.platform5 = os.ObjectSprite(self.player_base_image, 600, SCREEN_HEIGHT - 100, "platform")
+        self.platform6 = os.ObjectSprite(self.player_base_image, 700, SCREEN_HEIGHT - 120, "levitating platform")
+        self.platform7 = os.ObjectSprite(self.player_base_image, 600, 300, "platform")
+        self.platform8 = os.ObjectSprite(self.player_base_image, 500, 300, "platform")
+        self.platform8.image.fill(GREEN)
+        self.platform9 = os.ObjectSprite(self.player_base_image, 400, 350, "platform")
+        self.platform10 = os.ObjectSprite(self.player_base_image, 300, 400, "platform")
+        self.platform11 = os.ObjectSprite(self.player_base_image, 200, 450, "platform")
+        self.platform12 = os.ObjectSprite(self.player_base_image, 100, 500, "platform")
+        self.platform13 = os.ObjectSprite(self.player_base_image, 0, 450, "levitating platform")
+        self.platform14 = os.ObjectSprite(self.player_base_image, 100, 700, "platform")
+        self.door = os.ObjectSprite(self.player_base_image, 100, 570, "door")
+
+        self.object_group.add(self.platform)
+        self.object_group.add(self.platform2)
+        self.object_group.add(self.platform3)
+        self.object_group.add(self.platform4)
+        self.object_group.add(self.platform5)
+        self.object_group.add(self.platform6)
+        self.object_group.add(self.platform7)
+        self.object_group.add(self.platform8)
+        self.object_group.add(self.platform9)
+        self.object_group.add(self.platform10)
+        self.object_group.add(self.platform11)
+        self.object_group.add(self.platform12)
+        self.object_group.add(self.platform13)
+        self.object_group.add(self.platform14)
+        self.object_group.add(self.door)
+
+
         #self.player = ps.Player(0, 0, 105, 109, self.screen, self.player1_img)
         #create a group for the player
 
         self.player_group.add(self.player)
-        self.object_group.add(self.test_object)
+
+    #def platform_change(self, self.platform_name, color, new_x, new_y):
+        #self.platform_name.image.fill(color)
+        #self.platform_name.rect.x = new_x
+        #self.platform_name.rect.y = new_y
+
+
+    def change_level(self):
+        print("Change Level function is being called")
+        if self.player.map_level == 2:
+            print("Map level = 2")
+            self.player.rect.x = 0
+            self.player.rect.y = 900
+            self.bg_img = pygame.image.load('pictures/desertbg.jpeg')
+            self.bg_img = pygame.transform.scale(self.bg_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
+            self.bg_music = pygame.mixer.music.load("music/bg_musiclvl2.mp3")
+            pygame.mixer.music.play(-1)
+            self.platform_change(self.platform, BLUE, 300, 400)
+
+
 
     def run(self):
         while self.running:
             self.clock.tick(60)
             self.events()
-
+            self.player.collisiondetection(self.object_group)
             self.update()
             self.draw()
 
@@ -93,12 +150,17 @@ class Game:
     # This is where the game logic goes
     def update(self):
        self.player_group.update()
+       self.object_group.update()
        self.player.gravity()
+       self.player.collisiondetection(self.object_group)
+       if self.player.changing_level == True:
+           self.change_level()
+
 
     def draw(self):
         # set background to background image and draw it
        #set screen background white
-        self.screen.blit(bg_img, (0, 0))
+        self.screen.blit(self.bg_img, (0, 0))
         self.player_group.draw(self.screen) #draw player
         self.object_group.draw(self.screen) #draw object
         pygame.display.flip()  # update a portion of the screen
@@ -113,3 +175,4 @@ if __name__ == '__main__':
     game = Game()
     game.run()
     game.quit()
+
--- playersprite.py
+++ playersprite.py
@@ -23,8 +23,8 @@ class Player(pygame.sprite.Sprite):
         self.image_position = 0
         self.jump = False #boolean to check if player is jumping
         self.jump_level = 10
-        self.bg_music = pygame.mixer.music.load('music/bg_music.mp3')
-        pygame.mixer.music.play(-1)
+        self.map_level = 1
+        self.changing_level = False
 
     def flip_images(self, image_list):
         flipped_images = []
@@ -44,12 +44,14 @@ class Player(pygame.sprite.Sprite):
 
 
         if keys[pygame.K_LEFT]:
-            print(self.image_index)
-            self.rect.x = self.rect.x - self.speed
-            if self.image == self.left_image[3]:
-                self.image_index = 0
-            self.image_index = self.image_index + 1
-            self.image = self.left_image[self.image_index]
+
+                print(self.image_index)  # print the image index
+                self.rect.x -= self.speed  # move the player left
+                self.image_index = self.image_index + 1
+                if self.image_index > 3:  # if the image is the last image in the list
+                    self.image_index = 0  # set the image index to 0
+                    # increment the image index
+                self.image = self.left_image[self.image_index]
 
         if keys[pygame.K_RIGHT]:
             self.rect.x = self.rect.x + self.speed
@@ -64,9 +66,9 @@ class Player(pygame.sprite.Sprite):
             self.rect.y = self.rect.y + self.speed
             self.image = self.base_image
 
-        if keys[pygame.K_UP]:
-            self.rect.y = self.rect.y - self.speed
-            self.image = self.up_image[0]
+        #if keys[pygame.K_UP]:
+         #   self.rect.y = self.rect.y - self.speed
+          #  self.image = self.up_image[0]
 
         if self.jump == False:
             if keys[pygame.K_SPACE]:
@@ -91,5 +93,51 @@ class Player(pygame.sprite.Sprite):
 
         if self.rect.bottom > self.screenheight:
             self.rect.bottom = self.screenheight
+
+    def gravity(self):
+        if self.rect.y < self.screenheight - self.rect.height:  # if the player is not on the ground
+            self.rect.y += 3  # add 1 to the y position of the player to simulate gravity
+
+    def check_above_platform(self, object_rect):
+        if self.rect.bottom > object_rect.top:  # if the player is below the platform and the player is above the platform rect.bottom has to be greater than the top of the platform to mean that the player is above the platform
+            self.rect.bottom = object_rect.top
+
+    def check_below_platform(self, object_rect):
+        if object_rect.bottom > self.rect.top > object_rect.top and self.rect.right > object_rect.left and self.rect.left < object_rect.right:
+            self.rect.top = object_rect.bottom
+
+    def check_left_of_platform(self, object_rect):
+        if self.rect.right > object_rect.left and self.rect.right < object_rect.right and self.rect.bottom > object_rect.top and self.rect.top < object_rect.bottom:
+            # if the player jumps on the platform from the left side, move player to top left corner of platform
+            # if self.rect.bottom < object_rect.top:
+            #     self.rect.bottom = object_rect.top
+            # else:
+            self.rect.right = object_rect.left
+
+    def check_right_of_platform(self, object_rect):
+        if self.rect.left < object_rect.right and self.rect.left > object_rect.left and self.rect.bottom > object_rect.top and self.rect.top < object_rect.bottom:
+            self.rect.left = object_rect.right
+
+    def collisiondetection(self, object_group):
+        for object in object_group:
+            if self.rect.colliderect(object.rect):
+                if object.name == "platform" or object.name == "levitating platform" or object.name == "moving platform":
+                    if self.check_above_platform(object.rect):
+                        return
+                    elif self.check_below_platform(object.rect):
+                        return
+                    elif self.check_left_of_platform(object.rect):
+                        return
+                    elif self.check_right_of_platform(object.rect):
+                        return
+
+                elif object.name == "coin":
+                    object_group.remove(object)
+                elif object.name == "door":
+                    # if player is inside the door, move to next level
+                    if self.rect.x > object.rect.x and self.rect.x < object.rect.x + object.rect.width and self.rect.y > object.rect.y and self.rect.y < object.rect.y + object.rect.height:
+                        self.map_level += 1
+                        self.changing_level = True
+
 
 
